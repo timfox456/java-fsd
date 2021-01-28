049aae09f7b0b485398e382d0dd02a3d2ee252bc
Tim Fox <tim@elephantscale.com>
2021-01-28 12:58:09 -0500

    Added spring-log4j

---
+++ spring-log4j/src/test/java/com/example/springlog4j/SpringLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlog4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLog4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
