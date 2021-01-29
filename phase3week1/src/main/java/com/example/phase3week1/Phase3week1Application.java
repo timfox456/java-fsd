6ef1e6e36c722b7e93236bb28a58896446300798
Tim Fox <tim@elephantscale.com>
2021-01-29 12:59:37 -0500

    Fixed this for pom

--- phase3week1/src/main/java/com/example/phase3week1/Phase3week1Application.java
+++ phase3week1/src/main/java/com/example/phase3week1/Phase3week1Application.java
@@ -1,5 +1,8 @@
 package com.example.phase3week1;
 
+import com.example.phase3week1.controller.WelcomeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Phase3week1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Phase3week1Application.class, args);
 	}
 
