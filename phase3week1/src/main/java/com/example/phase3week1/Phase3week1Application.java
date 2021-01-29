4a5e7ac3fc7c3373ceb129f6382e5543f9a4c361
Tim Fox <tim@elephantscale.com>
2021-01-29 11:41:01 -0500

    Added phase3week1 to github -- clean run from spring intializr

---
+++ phase3week1/src/main/java/com/example/phase3week1/Phase3week1Application.java
@@ -0,0 +1,13 @@
+package com.example.phase3week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3week1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3week1Application.class, args);
+	}
+
+}
---
+++ phase3week1/src/test/java/com/example/phase3week1/Phase3week1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phase3week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3week1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
