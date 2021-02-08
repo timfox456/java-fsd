a5ebc4da38fe10d62291b120825e5d73aebcab64
Tim Fox <tim@elephantscale.com>
2021-02-08 10:37:46 -0500

    Added miscellaneous files

--- SpringKafka/src/main/java/com/example/SpringKafka/SpringKafkaApplication.java
+++ SpringKafka/src/main/java/com/example/SpringKafka/SpringKafkaApplication.java
@@ -3,11 +3,35 @@ package com.example.SpringKafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+
+
 @SpringBootApplication
 public class SpringKafkaApplication {
+
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaApplication.class, args);
 	}
+
+
+	@KafkaListener(topics = "ecommerce", groupId = "group-id")
+	public void listen(String message) {
+		System.out.println("Received Message in group - group-id: " + message);
+	}
+
 
 }
--- phase3week1/src/test/java/com/example/phase3week1/Phase3week1ApplicationTests.java
+++ phase3week1/src/test/java/com/example/phase3week1/Phase3week1ApplicationTests.java
@@ -1,7 +1,14 @@
 package com.example.phase3week1;
 
+import com.example.phase3week1.model.User;
+import com.example.phase3week1.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.util.List;
 
 @SpringBootTest
 class Phase3week1ApplicationTests {
@@ -9,5 +16,7 @@ class Phase3week1ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
