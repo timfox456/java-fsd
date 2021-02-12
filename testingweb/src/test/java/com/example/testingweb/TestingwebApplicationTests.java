d165a6556df1e1465733a570b82370c9bb84da1f
Tim Fox <tim@elephantscale.com>
2021-02-12 11:33:53 -0500

    Added testingweb applicaiton

---
+++ testingweb/src/test/java/com/example/testingweb/TestingwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
