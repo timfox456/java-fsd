2aaa95769afa715cb19620a7720bdeeefb358b70
Tim Fox <tim@elephantscale.com>
2021-02-12 11:57:09 -0500

    Added testingweb project

--- testingweb/src/test/java/com/example/testingweb/TestingwebApplicationTests.java
+++ testingweb/src/test/java/com/example/testingweb/TestingwebApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 @SpringBootTest
-class TestingwebApplicationTests {
+@AutoConfigureMockMvc
+
+public class TestingwebApplicationTests {
+
+
+
+	@Autowired
+	private HomeController controller;
+
+
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
+
+		assertThat(controller).isNotNull();
+
+	}
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
 	}
 
 }
